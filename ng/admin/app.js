67a3443e8308fb3e0f89aaa1be98b2e7aa33e3b9
Christopher Brown <io@henrian.com>
2015-02-02 14:04:04 -0600

    Add abstract routes; add multi-sentence import form; improve nav styling

--- ng/admin/app.js
+++ ng/admin/app.js
@@ -1,4 +1,4 @@
-/*jslint browser: true */ /*globals _, angular */
+/*jslint browser: true */ /*globals _, angular, stringifyResponse */
 var app = angular.module('adminApp', [
   'ngResource',
   'ngStorage',
@@ -16,25 +16,43 @@ app.config(function($stateProvider, $locationProvider, $urlRouterProvider) {
     templateUrl: '/ng/admin/login.html',
     controller: 'login',
   })
+  // sentences
   .state('sentences', {
-    url: '/sentences/',
+    url: '/sentences',
+    templateUrl: '/ng/admin/sentences/layout.html',
+    abstract: true,
+  })
+  .state('sentences.list', {
+    url: '/',
     templateUrl: '/ng/admin/sentences/list.html',
-    controller: 'sentences',
+    controller: 'sentences.list',
   })
-  .state('sentence', {
-    url: '/sentences/{id}',
+  .state('sentences.import', {
+    url: '/import',
+    templateUrl: '/ng/admin/sentences/import.html',
+    controller: 'sentences.import'
+  })
+  // matches /import, so edit must come after import
+  .state('sentences.edit', {
+    url: '/{id}',
     templateUrl: '/ng/admin/sentences/edit.html',
-    controller: 'sentence'
+    controller: 'sentences.edit'
   })
+  // participants
   .state('participants', {
-    url: '/participants/',
-    templateUrl: '/ng/admin/participants/list.html',
-    controller: 'participants',
+    url: '/participants',
+    templateUrl: '/ng/admin/participants/layout.html',
+    abstract: true,
   })
-  .state('participant', {
-    url: '/participants/{id}',
+  .state('participants.list', {
+    url: '/',
+    templateUrl: '/ng/admin/participants/list.html',
+    controller: 'participants.list',
+  })
+  .state('participants.edit', {
+    url: '/{id}',
     templateUrl: '/ng/admin/participants/edit.html',
-    controller: 'participant'
+    controller: 'participants.edit'
   });
 
   $locationProvider.html5Mode(true);
@@ -81,13 +99,13 @@ app.controller('login', function($scope, $flash, $window, AccessToken) {
   };
 });
 
-app.controller('sentences', function($scope, $flash, Sentence) {
+app.controller('sentences.list', function($scope, $flash, Sentence) {
   $scope.sentences = Sentence.query({active: true});
 
   $scope.delete = function(sentence) {
     var promise = sentence.$delete().then(function(res) {
       $scope.sentences.splice($scope.sentences.indexOf(sentence), 1);
-      return 'Deleted';
+      return 'Deleted sentence.';
     }, function(res) {
       return 'Error deleting sentence. ' + stringifyResponse(res);
     });
@@ -95,13 +113,13 @@ app.controller('sentences', function($scope, $flash, Sentence) {
   };
 });
 
-app.controller('sentence', function($scope, $flash, $state, Sentence) {
+app.controller('sentences.edit', function($scope, $flash, $state, Sentence) {
   $scope.sentence = Sentence.get($state.params);
 
   $scope.submit = function(ev) {
     var promise = $scope.sentence.$save().then(function(res) {
       $state.go('.', {id: $scope.sentence.id}, {notify: false});
-      return 'Saved.';
+      return 'Saved sentence.';
     }, function(res) {
       return 'Error saving sentence. ' + stringifyResponse(res);
     });
@@ -109,13 +127,42 @@ app.controller('sentence', function($scope, $flash, $state, Sentence) {
   };
 });
 
-app.controller('participants', function($scope, $flash, Participant) {
+app.controller('sentences.import', function($scope, $q, $flash, $state, Sentence) {
+  $scope.active = true;
+  $scope.language = 'en';
+
+  $scope.prepare = function(ev) {
+    var texts = $scope.input.trim().split(/\n/);
+    $scope.sentences = texts.map(function(text) {
+      return new Sentence({
+        text: text,
+        language: $scope.language,
+        active: $scope.active,
+      });
+    });
+  };
+
+  $scope.submit = function(ev) {
+    var promises = $scope.sentences.map(function(sentence) {
+      return sentence.$save();
+    });
+    var promise = $q.all(promises).then(function(res) {
+      return 'Inserted ' + res.length + ' sentences.';
+    }, function(res) {
+      return 'Error saving sentence. ' + stringifyResponse(res);
+    });
+    $flash(promise);
+  };
+});
+
+
+app.controller('participants.list', function($scope, $flash, Participant) {
   $scope.participants = Participant.query();
 
   $scope.delete = function(participant) {
     var promise = participant.$delete().then(function(res) {
       $scope.participants.splice($scope.participants.indexOf(participant), 1);
-      return 'Deleted';
+      return 'Deleted participant.';
     }, function(res) {
       return 'Error deleting participant. ' + stringifyResponse(res);
     });
@@ -123,7 +170,8 @@ app.controller('participants', function($scope, $flash, Participant) {
   };
 });
 
-app.controller('participant', function($scope, $flash, $state, Participant, Response) {
+
+app.controller('participants.edit', function($scope, $flash, $state, Participant, Response) {
   $scope.participant = Participant.get({id: $state.params.id});
   $scope.responses = Response.query({participant_id: $state.params.id});
 });
