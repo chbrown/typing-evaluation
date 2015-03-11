197e628c8c7a86d44ac056a2760e1206b4fca965
Christopher Brown <io@henrian.com>
2015-03-10 21:51:18 -0500

    Add cluster handling and domain wrapping for better process resilience

--- server.js
+++ server.js
@@ -1,11 +1,41 @@
+var cluster = require('cluster');
+var domain = require('domain');
 var http = require('http-enhanced');
 var logger = require('loge');
 
 var root_controller = require('./controllers');
 
-module.exports = http.createServer(function(req, res) {
-  logger.debug('%s %s', req.method, req.url);
-  root_controller(req, res);
+var server = module.exports = http.createServer(function(req, res) {
+  var request_domain = domain.create();
+  request_domain.add(req);
+  request_domain.add(res);
+  request_domain.on('error', function(err) {
+    logger.error('Domain: %s', err.stack);
+
+    // let the master know we're dying
+    cluster.worker.disconnect();
+
+    // close down after 10s so that we don't drop current connections.
+    setTimeout(function() {
+      process.exit(1);
+    }, 10000).unref();
+
+    // stop taking new requests
+    server.close();
+
+    try {
+      // try to send an error to the request that triggered the problem
+      res.die('Exception encountered! ' + err.toString());
+    }
+    catch (internal_error) {
+      // oh well, not much we can do at this point
+      logger.error('Error responding with error! %s', internal_error.stack);
+    }
+  });
+  request_domain.run(function() {
+    logger.debug('%s %s', req.method, req.url);
+    root_controller(req, res);
+  });
 })
 .on('listening', function() {
   var address = this.address();
