192488d1ec6a1d1aeb3aadf0da26163402112ff9
Christopher Brown <io@henrian.com>
2019-03-30 11:03:35 -0400

    Simplify ngResource config

--- ui/models.js
+++ ui/models.js
@@ -1,39 +1,9 @@
-/*jslint browser: true */ /*globals angular, cookies */
+const angular = require('angular')
 
-cookies.defaults = function() {
-  // root path; expires in a month
-  return {
-    path: '/',
-    expires: new Date(new Date().getTime() + 31 * 24 * 60 * 60 * 1000),
-  }
-};
-
-(function(angular) {
-  const app = angular.module('typing-evaluation-models', [
-    'ngResource',
-  ])
-
-  app.service('Sentence', ($resource) => {
-    return $resource('/api/sentences/:id', {
-      id: '@id',
-    })
-  })
-
-  app.service('Administrator', ($resource) => {
-    return $resource('/api/administrators/:id', {
-      id: '@id',
-    })
-  })
-
-  app.service('Participant', ($resource) => {
-    return $resource('/api/participants/:id', {
-      id: '@id',
-    })
-  })
-
-  app.service('Response', ($resource) => {
-    return $resource('/api/responses/:id', {
-      id: '@id',
-    })
-  })
-}(angular))
+angular.module('typing-evaluation-models', [
+  require('angular-resource'),
+]).
+service('Sentence', $resource => $resource('/api/sentences/:id', {id: '@id'})).
+service('Administrator', $resource => $resource('/api/administrators/:id', {id: '@id'})).
+service('Participant', $resource => $resource('/api/participants/:id', {id: '@id'})).
+service('Response', $resource => $resource('/api/responses/:id', {id: '@id'}))
