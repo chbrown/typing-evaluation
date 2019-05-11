b4f13a6ff3a065c8148f5c08081cc531e2ad6d95
Christopher Brown <io@henrian.com>
2019-05-11 01:08:17 -0400

    Modernize db config via env
    (docker no longer autogenerates linked containers like it used to)

--- db.js
+++ db.js
@@ -4,8 +4,8 @@ const {logger} = require('./util')
 
 // the DB_PORT_5432_TCP_* stuff comes from docker
 const db = new sqlcmd.Connection({
-  host: process.env.DB_PORT_5432_TCP_ADDR || '127.0.0.1',
-  port: process.env.DB_PORT_5432_TCP_PORT || '5432',
+  host: process.env.DB_HOST || '127.0.0.1',
+  port: process.env.DB_PORT || '5432',
   user: 'postgres',
   database: 'typing-evaluation',
 })
